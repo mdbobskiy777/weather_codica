4dcae878c495e39829373af0f40e141ecbdc3434
Eugene Bober <mdbobskiy777@gmail.com>
2022-06-16 09:37:49 +0300

    app init

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    'jest/globals': true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb/hooks',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'jest',
+  ],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/function-component-definition': 'off',
+    'react/prop-types': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+  },
+  globals: {
+    JSX: true,
+    React: true,
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+export const App = () => (
+  <div className="App">
+    App
+  </div>
+);
